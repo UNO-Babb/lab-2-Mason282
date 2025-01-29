611b2bfc59b61674e4f9e40c4a9ae547f7f17715
Mason282 <masonrodgers@unomaha.edu>
2025-01-29 16:45:17 -0600

    Done with lab
    Git hub would not let me submit

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,7 +1,7 @@
 #Magic8Ball.py
-#Name:
-#Date:
-#Assignment:
+#Name: Mason Rodgers
+#Date: 1/29/25
+#Assignment: 8 ball
 
 #We will need random for this program, import to use this package.
 import random
@@ -10,9 +10,22 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
+question=input("What do you desire" '?')
 
+answers=["As I see it, yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.",
+"Do not count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.",
+"Outlook not so good.","Outlook good.", "Reply hazy, try again.","Signs point to yes.", "Very doubtful.", "Without a doubt.",
+"Yes.", "Yes ,definitely.", "You may rely on it."]
   #Answer question randomly with one of the options from your earlier list.
+length=len(answers)
+r=random.random() * length
+
+r=int(r) 
+print(r)
+response=answers[r]
+print(response)
 
 
 if __name__ == '__main__':
   main()
+
